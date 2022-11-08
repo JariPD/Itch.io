1abdb9563595a91d67e549473920ec6c0d0ed3af
Jari Dijk <dijkjari@gmail.com>
2022-11-08 14:04:16 +0100

    blackjack prototype

---
+++ Itch.io/Assets/Jari/Scripts/UIManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public static UIManager instance;
+
+    [Header("References")]
+    [SerializeField] private BlackJackManager blackJackManager;
+
+    [Header("Text Objects")]
+    [SerializeField] private TextMeshProUGUI userCardValueText;
+    [SerializeField] private TextMeshProUGUI pulledCardText;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        userCardValueText.text = "Cards value: " + blackJackManager.UserTotalCardValue.ToString();
+    }
+    
+    public void UpdatePulledCardText(int cardValue)
+    {
+        pulledCardText.text = "You pulled: " + cardValue.ToString();
+    }
+}
