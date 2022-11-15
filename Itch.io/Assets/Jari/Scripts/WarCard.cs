47a2f1359fb5d06ebaacd864c182669d5f449831
Jari Dijk <dijkjari@gmail.com>
2022-11-15 16:28:19 +0100

    war card selecting

--- Itch.io/Assets/Jari/Scripts/WarCard.cs
+++ Itch.io/Assets/Jari/Scripts/WarCard.cs
@@ -9,21 +9,50 @@ public class WarCard : MonoBehaviour
     private int attack = 2;
     private int health = 3;
 
-    [Header("Anim")]
-    private Animator anim;
+    [Header("Card Settings")]
+    private Vector3 startPos;
+    [SerializeField] private bool cardSelected;
+    private bool validSpot = false;
+
+    [Header("Card Follow")]
+    [SerializeField] private float offset;
+    private Vector3 pos;
+
+    private float t;
+
 
     private void Start()
     {
-        anim = GetComponent<Animator>();
+        startPos = transform.position;
     }
 
-    private void OnMouseOver()
+    private void Update()
     {
-        anim.SetBool("Hover", true);
+        if (cardSelected)
+        {
+            pos = Input.mousePosition;
+            pos.z = offset;
+            transform.position = Camera.main.ScreenToWorldPoint(pos);
+            //transform.position = new Vector3(Camera.main.ScreenToWorldPoint(pos).x, Camera.main.ScreenToWorldPoint(pos).y, transform.position.z);
+        }
+
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) && !validSpot && cardSelected)
+        {
+            t++;
+            if (t >= 2)
+            {
+                t = 0;
+                cardSelected = false;
+                transform.position = startPos;
+            }
+        }
     }
 
-    private void OnMouseExit()
+    private void OnMouseDown()
     {
-        anim.SetBool("Hover", false);
+        if (!cardSelected)
+        {
+            cardSelected = true;
+        }
     }
 }
